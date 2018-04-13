c3fa4a39ff12b7e5d0a47fa2f4f0c9c9044f11c7
Kwaku J <gyazi@live.com>
2018-04-13 01:51:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function theBeatlesPlay(musicaians, instruments) {
-  var new = []
-  for(var i = 0, i++) {
-    console.log (musicians[i] + "plays" + instruments[i])
+function theBeatlesPlay(musicians, instruments) {
+  var yes = []
+  for(var i = 0; i < musicians.length; i++) {
+    console.log(musicians[i] + "plays" + instruments[i])
   }
+}
