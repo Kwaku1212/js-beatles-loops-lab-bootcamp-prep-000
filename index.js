3632099392d28f25243b7721f912e64492da5752
Kwaku J <gyazi@live.com>
2018-04-13 02:13:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function theBeatlesPlay(musicians, instruments) {
   var yes = []
   for(var i = 0; i < musicians.length; i++) {
-    console.log(`"${musicians[i]} plays ${instruments[i]}"`)
-  }
+    results = console.log(`"${musicians[i]} plays ${instruments[i]}"`)
+    yes.push(results)
+    }
+  return yes
 }
 
 var music = ["John", "Andy", "Bob"]
